fec37518723f003914f50cc85ea31ba31271ac85
Romeo Flauta <meoflauta@gmail.com>
2024-03-21 20:57:46 +0800

    Create skeleton for ListingItemView

---
+++ AirBNBSwiftUIClone/Core/Listings/ListingItemView.swift
@@ -0,0 +1,49 @@
+//
+//  ListingItemView.swift
+//  AirBNBSwiftUIClone
+//
+//  Created by Romeo Flauta on 3/21/24.
+//
+
+import SwiftUI
+
+struct ListingItemView: View {
+    var body: some View {
+        VStack(spacing: 8) {
+            // images
+            Rectangle()
+                .frame(height: 320)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+            // listing details
+            HStack(alignment: .top) {
+                // details
+                VStack(alignment: .leading) {
+                    Text("Miami, Florida")
+                        .fontWeight(.semibold)
+                    Text("12 mi away")
+                        .foregroundStyle(.gray)
+                    Text("Nov 3 - 10")
+                        .foregroundStyle(.gray)
+                    HStack(spacing: 4) {
+                        Text("$567")
+                            .fontWeight(.semibold)
+                        Text("night")
+                    }
+                }
+                Spacer()
+                //rating
+                HStack(spacing: 2) {
+                    Image(systemName: "star.fill")
+                    Text("4.86")
+                }
+            }
+            .font(.footnote)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ListingItemView()
+}
