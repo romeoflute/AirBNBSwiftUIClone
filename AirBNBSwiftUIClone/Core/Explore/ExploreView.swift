8ef6ffd359f8ade0c67db117e49873805bd6f7f1
Romeo Flauta <meoflauta@gmail.com>
2024-03-21 20:28:38 +0800

    Create skeleton of ExploreView

---
+++ AirBNBSwiftUIClone/Core/Explore/ExploreView.swift
@@ -0,0 +1,29 @@
+//
+//  ExploreView.swift
+//  AirBNBSwiftUIClone
+//
+//  Created by Romeo Flauta on 3/21/24.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 32) {
+                    ForEach(0..<10, id: \.self) { listing in
+                        Rectangle()
+                            .frame(height: 400)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
+                }
+                .padding()
+            }
+        }
+    }
+}
+
+#Preview {
+    ExploreView()
+}
