940669e65f967499c38f4057fb86c278e5d87d05
Romeo Flauta <meoflauta@gmail.com>
2024-03-21 18:15:50 +0800

    Initial Commit

---
+++ AirBNBSwiftUIClone/AirBNBSwiftUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBNBSwiftUICloneApp.swift
+//  AirBNBSwiftUIClone
+//
+//  Created by Romeo Flauta on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct AirBNBSwiftUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
