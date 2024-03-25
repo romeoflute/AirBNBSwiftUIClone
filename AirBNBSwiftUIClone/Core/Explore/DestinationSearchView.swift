a3c148e93597b53ca1be21a76017febee591c278
Romeo Flauta <meoflauta@gmail.com>
2024-03-25 11:54:41 +0800

    Complete UI of expanded and contracted states in DestinationSearchView

--- AirBNBSwiftUIClone/Core/Explore/DestinationSearchView.swift
+++ AirBNBSwiftUIClone/Core/Explore/DestinationSearchView.swift
@@ -7,52 +7,133 @@
 
 import SwiftUI
 
+enum DestinationSearchOptions {
+    case location
+    case dates
+    case guests
+}
+
 struct DestinationSearchView: View {
     @Binding var show: Bool
     @State private var destination = ""
+    @State private var selectedOption: DestinationSearchOptions = .location
+    @State private var startDate = Date()
+    @State private var endDate = Date()
+    @State private var numGuests = 0
     
     var body: some View {
         VStack(alignment: .leading) {
-            Button {
-                withAnimation(.snappy) {
-                    show = false
+            HStack {
+                Button {
+                    withAnimation(.snappy) {
+                        show = false
+                    }
+                } label: {
+                    Image(systemName: "xmark.circle")
+                        .imageScale(.large)
+                        .foregroundStyle(.black)
                 }
-            } label: {
-                Image(systemName: "xmark.circle")
-                    .resizable()
-                    .foregroundStyle(.black.opacity(0.4))
-                    .frame(width: 40, height: 40)
-                    .padding(.leading)
-            }
-            
-            VStack(alignment: .leading) {
-                Text("Where to?")
-                    .fontWeight(.semibold)
-                .font(.title2)
-                HStack {
-                    Image(systemName: "magnifyingglass")
-                        .imageScale(.small)
-                    TextField("Search destinations", text: $destination)
-                        .font(.headline)
-                        .foregroundStyle(.gray)
-                }
-                .frame(height: 44)
-                .padding(.horizontal)
-                .overlay {
-                    RoundedRectangle(cornerRadius: 8)
-                        .stroke(lineWidth: 1.0)
-                        .foregroundStyle(Color(.systemGray4))
+                Spacer()
+                if !destination.isEmpty {
+                    Button {
+                        destination = ""
+                    } label: {
+                        Text("Clear")
+                            .foregroundStyle(.black)
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                    }
                 }
             }
             .padding()
+            
+            VStack(alignment: .leading) {
+                if selectedOption == .location {
+                    Text("Where to?")
+                        .fontWeight(.semibold)
+                    .font(.title2)
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .imageScale(.small)
+                        TextField("Search destinations", text: $destination)
+                            .font(.headline)
+                            .foregroundStyle(.gray)
+                    }
+                    .frame(height: 44)
+                    .padding(.horizontal)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(lineWidth: 1.0)
+                            .foregroundStyle(Color(.systemGray4))
+                    }
+                } else {
+                    CollapsedPickerView(title: "Where", description: "Add destination")
+                }
+            }
+            .padding()
+            .frame(height: selectedOption == .location ? 120 : 64)
             .background(.white)
             .clipShape(RoundedRectangle(cornerRadius: 12))
             .padding()
             .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            .onTapGesture {
+                withAnimation(.snappy) { selectedOption = .location }
+            }
             
-            CollapsedPickerView(title: "When", description: "Add dates")
+            VStack(alignment: .leading) {
+                if selectedOption == .dates {
+                    Text("When's your trip?")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    VStack {
+                        DatePicker("From", selection: $startDate, displayedComponents: .date)
+                        Divider()
+                        DatePicker("To", selection: $endDate, displayedComponents: .date)
+                    }
+                    .foregroundStyle(.gray)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                } else {
+                    CollapsedPickerView(title: "When", description: "Add dates")
+                }
+            }
+            .padding()
+            .frame(height: selectedOption == .dates ? 180 : 64)
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding()
+            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            .onTapGesture {
+                withAnimation(.snappy) { selectedOption = .dates }
+            }
             
-            CollapsedPickerView(title: "Who", description: "Add guests")
+            VStack(alignment: .leading) {
+                if selectedOption == .guests {
+                    Text("Who's coming?")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    Stepper {
+                        Text("\(numGuests) adults")
+                    } onIncrement: {
+                        numGuests += 1
+                    } onDecrement: {
+                        guard numGuests > 0 else { return }
+                        numGuests -= 1
+                    }
+                } else {
+                    CollapsedPickerView(title: "Who", description: "Add guests")
+                        
+                }
+            }
+            .padding()
+            .frame(height: selectedOption == .guests ? 120 : 64)
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding()
+            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            .onTapGesture {
+                withAnimation(.snappy) { selectedOption = .guests }
+            }
             
             Spacer()
         }
@@ -79,10 +160,10 @@ struct CollapsedPickerView: View {
             .fontWeight(.semibold)
             .font(.subheadline)
         }
-        .padding()
-        .background(.white)
-        .clipShape(RoundedRectangle(cornerRadius: 12))
-        .padding()
-        .shadow(radius: 10)
+//        .padding()
+//        .background(.white)
+//        .clipShape(RoundedRectangle(cornerRadius: 12))
+//        .padding()
+//        .shadow(radius: 10)
     }
 }
